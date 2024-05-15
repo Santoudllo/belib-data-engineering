52792390ccda5f5e8bdf9a20b48de9e7c5983e9e
santoudllo <mamadoualimou.diallo@ynov.com>
2024-05-15 21:27:45 +0200

    script de tes avec l'api

--- script/getApi.py
+++ script/getApi.py
@@ -8,21 +8,27 @@ class BelibAPIClient:
         try:
             response = requests.get(self.api_url, params={'limit': limit})
             if response.status_code == 200:
-                return response.json()
+                json_data = response.json()
+                total_records = json_data.get('total', None)
+                return json_data, total_records
             else:
                 print(f"Failed to fetch data. Status code: {response.status_code}")
-                return None
+                return None, None
         except Exception as e:
             print(f"An error occurred: {e}")
-            return None
+            return None, None
 
 def main():
-    api_url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/belib-points-de-recharge-pour-vehicules-electriques-donnees-statiques/records"
+    api_url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/belib-points-de-recharge-pour-vehicules-electriques-disponibilite-temps-reel/records"
     client = BelibAPIClient(api_url)
-    data = client.fetch_data()
+    data, total_records = client.fetch_data()
     if data:
         print("Data fetched successfully:")
         print(data)
+        if total_records is not None:
+            print(f"Total records: {total_records}")
+        else:
+            print("Total records count not available.")
     else:
         print("Failed to fetch data.")
 
