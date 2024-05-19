a381cbd1c7591806e1a5da62a344bc4689ce95d7
santoudllo <mamadoualimou.diallo@ynov.com>
2024-05-19 13:09:08 +0200

    Exploration des donnée

---
+++ script/connexion_mongodb.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+import pandas as pd
+
+class BikeDataPipeline:
+    def __init__(self):
+        self.username = 'santoudllo'
+        self.password = '@Santou20'  # Remplacez par votre mot de passe réel
+        self.dbname = 'belib_database'
+        self.collection_name = 'belib.collection'
+        self.mongodb_uri = f"mongodb+srv://{quote_plus(self.username)}:{quote_plus(self.password)}@bike.kvgkgvj.mongodb.net/{self.dbname}?retryWrites=true&w=majority"
+
+    def fetch_data_from_mongodb(self):
+        try:
+            # Connect to MongoDB
+            client = MongoClient(self.mongodb_uri)
+            print("Connexion à MongoDB réussie.")
+            
+            # Access the database
+            db = client[self.dbname]
+            print(f"Accès à la base de données '{self.dbname}' réussi.")
+            
+            # Access the collection
+            collection = db[self.collection_name]
+            print(f"Accès à la collection '{self.collection_name}' réussi.")
+            
+            # Fetch the data
+            data = list(collection.find({}))
+            if not data:
+                print("Aucun document trouvé dans la collection.")
+            else:
+                print(f"{len(data)} documents trouvés dans la collection.")
+            
+            return data
+        
+        except Exception as e:
+            print(f"Erreur lors de la récupération des données depuis MongoDB : {e}")
+            return None
